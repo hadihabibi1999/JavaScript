4cf1f4dd99761a20681e848139a36310e08d92a7
HadiCyb <hadihabibi2000@gmail.com>
2019-02-04 16:36:29 +0400

    js-1

---
+++ practices/index.js
@@ -0,0 +1,6 @@
+//intraction:  alert - prompt - confirm
+let permission = confirm("are you a user?");
+if(permission==true){
+  let asking =  prompt("enter your name :","");2
+    alert(`Hello ${asking} ...`) 
+}else alert("get out");
