514afbb439bde2f6b947a525d20cdfab4b65a68c
HadiCyb <hadihabibi2000@gmail.com>
2019-02-07 23:59:37 +0400

    clock

--- practices/index.js
+++ practices/index.js
@@ -1,6 +1,22 @@
-//intraction:  alert - prompt - confirm
-let permission = confirm("are you a user?");
-if(permission==true){
-  let asking =  prompt("enter your name :","");2
-    alert(`Hello ${asking} ...`) 
-}else alert("get out");
+function changeTime() {
+    let time = new Date();
+    let hr  = time.getHours();
+    let mn = time.getMinutes();
+    let sc = time.getSeconds();
+    
+    hr=checkTime('hr');
+    mn=checkTime('mn');
+    sc=checkTime('sc');
+
+    document.getElementById("hr").innerHTML=hr;
+    document.getElementById("mn").innerHTML=mn;
+    document.getElementById("sc").innerHTML=sc;
+
+    let t = setTimeout("changeTime",500)
+
+    function checkTime(id) {
+        if (id<10)
+        id='0'+id;
+        return id;
+    }
+}
