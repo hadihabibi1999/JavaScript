304fb0d4777d2cd059723056ea36bb872f208a62
HadiCyb <hadihabibi2000@gmail.com>
2019-02-12 22:42:59 +0400

    up to date

--- practices/index.js
+++ practices/index.js
@@ -1,23 +1,22 @@
-
 function startTime() {
 
-  let date = new Date();
-  let h = date.getHours();
-  let m = date.getMinutes();
-  let s = date.getSeconds();
-
-  m = checkTime(m);
-  s = checkTime(s);
-
-  document.getElementById('h').innerHTML = h;
-  document.getElementById('m').innerHTML = m;
-  document.getElementById('s').innerHTML = s;
-
-  setTimeout(startTime, 1000);
-}
-
-function checkTime(i) {
-  if (i < 10) 
-  i='0'+i;
-  return i;
-}
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+  
+    m = checkTime(m);
+    s = checkTime(s);
+  
+    document.getElementById('h').innerHTML = h;
+    document.getElementById('m').innerHTML = m;
+    document.getElementById('s').innerHTML = s;
+  
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) 
+    i='0'+i;
+    return i;
+  }
