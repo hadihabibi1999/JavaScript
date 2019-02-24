748bb7573958bdf1a6173e0a28eb12993b0c111d
HadiCyb <hadihabibi2000@gmail.com>
2019-02-24 20:51:55 +0400

    solved problems

--- practices/index.js
+++ practices/index.js
@@ -19,12 +19,12 @@ class A {
   
 
   /*
-   1) class B is child od class A 
+   1) class B is child of class A 
    2) constructor is the 1st function whitch is called 
-   with 'new' key :{
+   3) 'new' key :{
        1)it makes an empty object
        2)calls constructor function 
        3)inputs all properties into it 
        4)returns all of them }
-   3)in super we transfer props from child (B) to (A) , (A) gets it by constructor();    
+   4)in super we transfer props from child (B) to (A) , (A) gets it by constructor();    
   */
