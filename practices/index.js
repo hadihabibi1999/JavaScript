31941e49763ce4475c763ce42c5678a27a53adda
HadiCyb <hadihabibi2000@gmail.com>
2019-03-05 20:41:49 +0400

    this keyWord

--- practices/index.js
+++ practices/index.js
@@ -12,3 +12,31 @@ let promise = new Promise(resolve => {
   setTimeout(() => fresolve("hi world ..."), 1000)
 });
 promise.then(alert); 
+
+
+/* let user = {
+  name: "John",
+  hi() {
+     console.log(this.name)
+      }
+}
+
+ let ss = user.hi;
+ ss();
+
+ */
+let user = { name: "John" };
+
+let admin = { name: "Admin",
+  sayHi() {
+    console.log( this.name );
+} };
+
+
+user.cc = admin.sayHi();//admin
+user.cc;
+
+user.cc = admin.sayHi;//john
+user.cc();
+
+
